f0280d0566e82131f93ef23f9943b8a8c9306873
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-02-14 15:28:27 -0300

    Search component
    The lookup component is a simple input field that allows the user to
    search for a location.
    The search component is located in the
    components folder being called search.js.

--- components/layout/article.js
+++ components/layout/article.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import { motion } from 'framer-motion'
 
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 20 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: -0, y: 20 }
+}
 
 const Layout = ({ children, title }) => {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,52 @@
+import Layout from "@/components/layout/article";
+
+import { SearchIcon, } from '@chakra-ui/icons'
+import {
+  Box,
+  HStack,
+  Center,
+  VStack,
+  Input,
+  InputGroup,
+  InputLeftElement
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <>
-  
-    </>
+    <Layout>
+      <Center h="100vh" >
+        <VStack
+          w="55vw"
+          h="65vh"
+          bgColor="white"
+          borderRadius="xl"
+          boxShadow="md"
+          p="5"
+        >
+          <Center>
+            <InputGroup >
+              <InputLeftElement pointerEvents='none'>
+                <SearchIcon color='gray.400' />
+              </InputLeftElement>
+              <Input
+                type='text'
+                placeholder='Enter Your Location'
+                w='100%'
+                color='black'
+                borderColor='gray.400'
+                borderRadius='xl'
+                borderWidth='2px'
+                _focus={{ outline: 'none' }}
+                _hover={{ borderColor: 'purple.500' }}
+                _placeholder={{ color: 'gray.400' }}
+                boxShadow='lg'
+
+              />
+            </InputGroup>
+          </Center>
+
+        </VStack>
+      </Center>
+    </Layout>
   )
 }
