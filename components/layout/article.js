db6f822200f41f03e43c26a7a3c398a109d0332d
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-02-16 11:15:09 -0300

    Update article.js

--- components/layout/article.js
+++ components/layout/article.js
@@ -26,8 +26,28 @@ const Layout = ({ children, title }) => {
         <meta name="keywords" content="weather, app, nextjs, chakra-ui" />
         <meta name="robots" content="index, follow" />
         <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
 
         <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Weather App" />
+        <meta property="og:description" content="Weather App" />
+        <meta property="og:url" content="https://weather-app-joaogustavo.vercel.app/" />
+        <meta property="og:site_name" content="Weather App" />
+        <meta property="og:image" content="/images/og-image.png" />
+        <meta property="og:image:secure_url" content="/images/og-image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Weather App" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Weather App" />
+        <meta name="twitter:title" content="Weather App" />
+        <meta name="twitter:image" content="/images/og-image.png" />
+        <meta name="twitter:image:alt" content="Weather App" />
+
       </Head>
       {children}
     </motion.article>
