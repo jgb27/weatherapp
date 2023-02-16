b418492a1e12b13a7c1085d6d8490864cdf13736
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-02-16 15:06:49 -0300

    Switch color of footer
    This is a test commit to see if I can push to the repo.

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import { Text } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Text color="#121212">
+    <Text color="#6e6e6e">
       &copy; {new Date().getFullYear()} João Gustavo Soares Bispo.
       <a href='https://github.com/jgbispo/weatherapp' target="_blank" rel="noreferrer"> GitHub</a>
     </Text>
